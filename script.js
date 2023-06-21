311cb1e02987c5265c7940da7073a489dfeb2613
vishalbhoyar24 <124866977+vishalbhoyar24@users.noreply.github.com>
2023-06-21 11:49:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let div = document.createElement("div");
+div.setAttribute("id", "browser-info");
+let browserName = navigator.appName;
+let version = navigator.appVersion;
+div.textContent =  "You are using " + browserName + " version " + version
+document.body.append(div);
